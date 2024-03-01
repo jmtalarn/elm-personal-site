1920ea1398609e1929cd8d3704829f043c906b24
jmtalarn <jmtalarn@gmail.com>
2024-03-01 01:44:33 +0100

    Added SVG support

--- app/Route/Blog.elm
+++ app/Route/Blog.elm
@@ -3,10 +3,12 @@ module Route.Blog exposing (ActionData, Data, Model, Msg, route)
 import BackendTask exposing (BackendTask)
 import Components.BlogPostCard exposing (blogPostCard)
 import DataModel.BlogPosts exposing (..)
+import Date
 import FatalError exposing (FatalError)
 import Head
 import Head.Seo as Seo
 import Html
+import Html.Attributes as Attribute
 import Pages.Url
 import PagesMsg exposing (PagesMsg)
 import RouteBuilder exposing (App, StatelessRoute)
@@ -69,14 +71,47 @@ head app =
         |> Seo.website
 
 
+blogPostGridStyle : List (Html.Attribute msg)
+blogPostGridStyle =
+    [ Attribute.style "column-count" "auto"
+    , Attribute.style "column-gap" "5px"
+    , Attribute.style "column-width" "25rem"
+    ]
+
+
+
+-- [ Attribute.style "display" "grid"
+-- , Attribute.style "grid-template-rows" "auto"
+-- , Attribute.style "grid-template-columns" "repeat(auto-fit, minmax(20rem, 1fr))"
+-- , Attribute.style "grid-gap" "1rem"
+-- , Attribute.style "grid-auto-flow" "dense"
+-- ]
+
+
 view :
-    App Data ActionData RouteParams
+    App (List BlogPost) ActionData RouteParams
     -> Shared.Model
     -> View (PagesMsg Msg)
 view app shared =
+    let
+        blogPosts =
+            List.sortWith
+                (\a b ->
+                    case compare (Date.toRataDie a.date) (Date.toRataDie b.date) of
+                        LT ->
+                            GT
+
+                        EQ ->
+                            EQ
+
+                        GT ->
+                            LT
+                )
+                app.data
+    in
     { title = "elm-pages is running"
     , body =
         [ Html.h1 [] [ Html.text "This is the blog index" ]
-        , Html.div [] (List.map blogPostCard app.data)
+        , Html.div blogPostGridStyle (List.map blogPostCard blogPosts)
         ]
     }
