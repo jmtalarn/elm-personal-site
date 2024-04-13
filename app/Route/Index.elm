b030ec5d264d8d84fe5a606f50646857e642a772
jmtalarn <jmtalarn@gmail.com>
2024-04-14 01:50:08 +0200

    Home index stuff

--- app/Route/Index.elm
+++ app/Route/Index.elm
@@ -1,11 +1,13 @@
-module Route.Index exposing (ActionData, Data, Model, Msg, route)
+module Route.Index exposing (ActionData, Data, Model, Msg, route, view)
 
 import BackendTask exposing (BackendTask)
+import Components.Home exposing (..)
 import FatalError exposing (FatalError)
 import Head
 import Head.Seo as Seo
-import Html
+import Html exposing (Html)
 import Html.Attributes as Attribute
+import List exposing (reverse)
 import Pages.Url
 import PagesMsg exposing (PagesMsg)
 import Route
@@ -74,19 +76,6 @@ head app =
         |> Seo.website
 
 
-caretoStyle : List (Html.Attribute msg)
-caretoStyle =
-    [ Attribute.style "border-radius" "50%"
-
-    -- , Attribute.style "border" "2px dotted salmon"
-    -- , Attribute.style "box-shadow" "5px 5px 10px Dimgray"
-    , Attribute.style "position" "absolute"
-
-    --, Attribute.style "width" "500px"
-    --, Attribute.style "height" "200px"
-    ]
-
-
 view :
     App Data ActionData RouteParams
     -> Shared.Model
@@ -94,105 +83,20 @@ view :
 view app shared =
     { title = "Joan Maria Talarn ~ web developer"
     , body =
-        [ Html.h1 [] [ Html.text "This is me! " ]
-        , Html.p []
-            [ Html.text <| "The message is: " ++ app.data.message
-            ]
+        [ ribbon "This is me!"
         , Html.div
-            [ Attribute.style "position" "relative"
-            , Attribute.style "width" "500px"
-            , Attribute.style "border-radius" "50%"
+            [ Attribute.style "box-shadow" "0 0 15px rgba(0,0,0,.1)"
+            , Attribute.style "padding" "2rem"
+            , Attribute.style "margin" "2rem 0rem"
             ]
-            [ animatedSvgBackground
-                [ Attribute.style "position" "absolute"
-                , Attribute.style "height" "600px"
-                , Attribute.style "width" "800px"
-                , Attribute.style "left" "-10rem"
-                , Attribute.style "top" "-3rem"
-                , Attribute.style "filter" "blur(40px)"
-                ]
-            , Html.img (Attribute.src "/images/home/careto_trucat-removebg-preview.png" :: caretoStyle) []
+            [ hero
+            , blog
+            , cv
+            , book
             ]
-        , Html.ul []
-            [ Html.li []
-                [ Route.Blog__Page__ { page = Nothing }
-                    |> Route.link [] [ Html.text "Web dev notes" ]
-                ]
-            , Html.li []
-                [ Route.Cv
-                    |> Route.link [] [ Html.text "CV" ]
-                ]
-            ]
+
+        -- , Html.p []
+        --     [ Html.text <| "The message is: " ++ app.data.message
+        --     ]
         ]
     }
-
-
-animatedSvgBackground : List (Html.Attribute msg) -> Html.Html msg
-animatedSvgBackground style =
-    svg
-        ([ SvgAttr.viewBox "0 0 800 500"
-         , SvgAttr.preserveAspectRatio "none"
-         , SvgAttr.width "100%"
-         , SvgAttr.id "blobSvg"
-         ]
-            ++ style
-        )
-        [ Svg.g
-            [ SvgAttr.transform "translate(174.31295776367188, 21.84337615966797)"
-            ]
-            [ Svg.defs []
-                [ Svg.filter
-                    [ SvgAttr.id "hue-rotate"
-                    ]
-                    [ Svg.feColorMatrix
-                        [ SvgAttr.type_ "hueRotate"
-                        , SvgAttr.values "0"
-                        ]
-                        [ Svg.animate
-                            [ SvgAttr.attributeName "values"
-                            , SvgAttr.dur "20s"
-                            , SvgAttr.from "0"
-                            , SvgAttr.to "360"
-                            , SvgAttr.repeatCount "indefinite"
-                            ]
-                            []
-                        ]
-                    ]
-                , Svg.linearGradient
-                    [ SvgAttr.id "gradient"
-                    , SvgAttr.x1 "0%"
-                    , SvgAttr.y1 "0%"
-                    , SvgAttr.x2 "0%"
-                    , SvgAttr.y2 "100%"
-                    ]
-                    [ Svg.stop
-                        [ SvgAttr.offset "0%"
-                        , SvgAttr.stopColor "rgb(255, 95, 109)"
-                        ]
-                        []
-                    , Svg.stop
-                        [ SvgAttr.offset "100%"
-                        , SvgAttr.stopColor "rgb(255, 195, 113)"
-                        ]
-                        []
-                    ]
-                ]
-            , path
-                [ SvgAttr.class "blob"
-                , SvgAttr.fill "url(#gradient)"
-                , SvgAttr.filter "url(#hue-rotate)"
-                ]
-                [ Svg.animate
-                    [ SvgAttr.attributeName "d"
-                    , SvgAttr.dur "10s"
-                    , SvgAttr.repeatCount "indefinite"
-                    , SvgAttr.values """M390.5,305Q314,360,245.5,368Q177,376,95,313Q13,250,80.5,162Q148,74,259,58Q370,42,418.5,146Q467,250,390.5,305Z;
-                                        M398,340.5Q354,431,240.5,446.5Q127,462,100,356Q73,250,122.5,182Q172,114,272.5,75.5Q373,37,407.5,143.5Q442,250,398,340.5Z;
-                                        M394.5,320Q331,390,249.5,390.5Q168,391,92.5,320.5Q17,250,78.5,154.5Q140,59,232,90Q324,121,391,185.5Q458,250,394.5,320Z;
-                                        M354,308Q317,366,234,394Q151,422,106,336Q61,250,110.5,172Q160,94,259.5,78Q359,62,375,156Q391,250,354,308Z;
-                                        M390.5,305Q314,360,245.5,368Q177,376,95,313Q13,250,80.5,162Q148,74,259,58Q370,42,418.5,146Q467,250,390.5,305Z;"""
-                    ]
-                    []
-                ]
-            ]
-        ]
