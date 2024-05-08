dc22713ab9b6840ba7596ca630107682d64fe878
jmtalarn <jmtalarn@gmail.com>
2024-05-08 19:58:28 +0200

    Headers for Amazon Request

--- app/Route/Book.elm
+++ app/Route/Book.elm
@@ -8,18 +8,26 @@ module Route.Book exposing (Model, Msg, RouteParams, route, Data, ActionData)
 
 import BackendTask exposing (BackendTask)
 import BackendTask.Http
+import BackendTask.Time
 import Components.Book exposing (book3Danimated)
 import Components.Home exposing (antonFontAttributeStyle, workSansAttributeStyle)
 import Components.Ribbon exposing (ribbon)
+import Date
+import Dict
 import FatalError exposing (FatalError)
 import Head
 import Head.Seo as Seo
 import Html
 import Html.Attributes as Attribute
+import Iso8601
+import LanguageTag
+import LanguageTag.Language
+import Pages
 import Pages.Url
 import PagesMsg exposing (PagesMsg)
 import RouteBuilder exposing (App, StatelessRoute)
 import Shared
+import Time
 import UrlPath
 import View exposing (View)
 
@@ -93,15 +101,35 @@ type alias Data =
 -- }
 
 
-getAmazonData : BackendTask (FatalError Error) String
+getAmazonData : BackendTask FatalError String
 getAmazonData =
+    let
+        now =
+            String.concat [ Maybe.withDefault "" (List.head <| String.split "." <| String.replace ":" "" <| String.replace "-" "" (Iso8601.fromTime <| Pages.builtAt)), "Z" ]
+
+        _ =
+            Debug.log "now" now
+
+        headers =
+            Dict.fromList
+                [ ( "Host", "webservices.amazon.es" )
+                , ( "X-Amz-Date", now )
+                , ( "X-Amz-Target", "com.amazon.paapi5.v1.ProductAdvertisingAPIv1.GetItems" )
+                , ( "Content-Encoding", "amz-1.0" )
+                ]
+    in
     BackendTask.Http.get
         "https://api.github.com/repos/dillonkearns/elm-pages"
         BackendTask.Http.expectString
+        |> BackendTask.allowFatal
 
 
 data : BackendTask FatalError Data
 data =
+    let
+        _ =
+            Debug.log "getAmazonData" getAmazonData
+    in
     BackendTask.succeed {}
 
 
@@ -114,7 +142,7 @@ head :
     App Data ActionData RouteParams
     -> List Head.Tag
 head app =
-    Seo.summary
+    (Seo.summary
         { canonicalUrlOverride = Nothing
         , siteName = title
         , image =
@@ -123,11 +151,16 @@ head app =
             , dimensions = Nothing
             , mimeType = Nothing
             }
-        , description = "My book fundamentos web"
+        , description = "Fundamentos web. Libro autopublicado en Amazon. "
         , locale = Nothing
         , title = title
         }
         |> Seo.website
+    )
+        ++ [ LanguageTag.Language.es
+                |> LanguageTag.build LanguageTag.emptySubtags
+                |> Head.rootLanguage
+           ]
 
 
 view :
@@ -145,10 +178,19 @@ view app shared =
             , Attribute.style "max-width" "1024px"
             ]
             [ Html.h2
-                [ Attribute.style "margin-left" "4rem" ]
+                [ Attribute.style "margin" "2rem 4rem" ]
                 [ Html.span antonFontAttributeStyle
-                    [ Html.text "Fundamentos web - el libro" ]
+                    [ Html.text "Fundamentos web" ]
                 ]
+            , Html.h3
+                [ Attribute.style "margin" "2rem 4rem" ]
+                [ Html.span
+                    (antonFontAttributeStyle
+                        ++ [ Attribute.style "font-size" "1.5rem" ]
+                    )
+                    [ Html.text "Fundamentos y conceptos básicos sobre el desarrollo web y manual práctico de la especificación de HTML, Javascript y CSS." ]
+                ]
+            , Html.p workSansAttributeStyle [ Html.text """Este libro es una guía introductoria a una serie de conceptos, técnicas y herramientas de los conceptos más básicos del desarrollo web. Viajaremos desde los conceptos y siglas más teóricos para introducirnos posteriormente en los aspectos más técnicos de los tres pilares del desarrollo web: HTML, Javascript y CSS.""" ]
             , book3Danimated False
             , ribbon "Book"
             ]
